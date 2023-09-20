7a3701b0af37cea5e169463daeb83fddc954e679
José Jaime <rpjosejaime@gmail.com>
2023-09-20 13:12:50 -0600

    Asignacion de roles

---
+++ database/migrations/2023_09_20_174758_create_roles.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        /*Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });*/
+
+        $role1 = Role::create(['name' => 'jefe']);
+        $role2 = Role::create(['name' => 'prefecto']);
+        $role3 = Role::create(['name' => 'admin']);
+        $user = User::find(1);
+        $user->assignRole($role1);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+       // Schema::dropIfExists('roles');
+    }
+};
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,26 +17,61 @@
                 </div>
                 <!--if vaidarRoles-->
                 <!--if vaidarRoles-->
-                <div class="col-sm-4 ">
-                </div>
+                    @hasrole('jefe')
+                        <div class="col-sm-4 ">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class=""><i class="fas fa-file"></i>
+                                        Reportes prefectura
+                                    </h5>
+                                    <p class="card-text">Consulta la asistencia de los profesores a las clases.
+                                    </p>
+                                    <a href="https://cetech.sjuanrio.tecnm.mx/docente/list_nivel_academico"
+                                        class="btn btn-primary">Acceder
+                                        <i class="fa fa-arrow-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    @else
+                    <div class="col-sm-4 "> </div>
+
+                    @endhasrole
                 <!--if sub 14-->
                 <!--if vaidarRoles-->
                 <!--if vaidarRoles-->
                 <!--if vaidarRoles-->
-                <div class="col-sm-4 ">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class=""><i class="fas fa-check"></i>
-                                Asistencia Docente
-                            </h5>
-                            <p class="card-text">Ingresa para iniciar el recorrido y confirmar asistencia.
-                            </p>
-                            <a href="https://cetech.sjuanrio.tecnm.mx/docente/list_nivel_academico"
-                                class="btn btn-primary">Acceder
-                                <i class="fa fa-arrow-right"></i></a>
+                @role('prefecto')
+                    <div class="col-sm-4 ">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class=""><i class="fas fa-check"></i>
+                                    Asistencia Docente
+                                </h5>
+                                <p class="card-text">Ingresa para iniciar el recorrido y confirmar asistencia.
+                                </p>
+                                <a href="https://cetech.sjuanrio.tecnm.mx/docente/list_nivel_academico"
+                                    class="btn btn-primary">Acceder
+                                    <i class="fa fa-arrow-right"></i></a>
+                            </div>
                         </div>
                     </div>
-                </div>
+                @endrole
+                @role('jefe')
+                    <div class="col-sm-4 ">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class=""><i class="fas fa-user-check"></i>
+                                    Administrar Prefectos
+                                </h5>
+                                <p class="card-text">Agrega, elimina o modifica información de los prefectos.
+                                </p>
+                                <a href="https://cetech.sjuanrio.tecnm.mx/docente/list_nivel_academico"
+                                    class="btn btn-primary">Acceder
+                                    <i class="fa fa-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                @endrole
                 <!--if sub 14-->
                 <!--if vaidarRoles-->
                 <!--if vaidarRoles-->
